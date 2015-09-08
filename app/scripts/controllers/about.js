1f73ae758e665ffc4001c6efe9b456c3cdfba889
smithnicholas17 <smith.nicholas90@gmail.com>
2015-09-08 15:01:39 +1000

    init
    boilerplate yeoman-generated angular scaffolding

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marsWeatherAppApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the marsWeatherAppApp
+ */
+angular.module('marsWeatherAppApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name marsWeatherAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the marsWeatherAppApp
+ */
+angular.module('marsWeatherAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
