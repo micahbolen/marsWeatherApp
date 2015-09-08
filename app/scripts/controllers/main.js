cf34a92718def009fee9cebe2ec408710e2be8b4
Micah Delane Bolen <micah.bolen@gmail.com>
2015-09-08 13:32:30 -0500

    Implement initial GET request from MAAS API.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,18 @@
  */
 angular.module('marsWeatherAppApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.latestReport = [];
+    $scope.error = [];
+    
+    $http.get('http://marsweather.ingenology.com/v1/latest/').
+      then(function(response) {
+      // this callback will be called asynchronously
+      // when the response is available
+      $scope.latestReport = angular.fromJson(response);
+      }, function(response) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      $scope.error = angular.fromJson(response);
+    });
+    
   });
